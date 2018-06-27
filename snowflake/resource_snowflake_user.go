b67acb611719cc79c10bc1e094ba7593e1758cf1
Greg Butzi <gregory.butzi@snowflake.net>
2018-06-27 08:36:14 -0700

    Added support for login_name and email; implemented update

--- snowflake/resource_snowflake_user.go
+++ snowflake/resource_snowflake_user.go
@@ -35,6 +35,17 @@ func resourceSnowflakeUser() *schema.Resource {
 					return strings.ToUpper(v.(string))
 				},
 			},
+			"email": {
+				Type:     schema.TypeString,
+				Optional: true,
+				StateFunc: func(v interface{}) string {
+					return strings.ToUpper(v.(string))
+				},
+			},
+			"must_reset_password": {
+				Type:     schema.TypeBool,
+				Optional: true,
+			},
 		},
 	}
 }
@@ -43,12 +54,14 @@ func resourceSnowflakeUserCreate(d *schema.ResourceData, meta interface{}) error
 	db := meta.(*sql.DB)
 	name := strings.ToUpper(d.Get("name").(string))
 	login_name := strings.ToUpper(d.Get("login_name").(string))
+	email := strings.ToUpper(d.Get("email").(string))
 
 	statement := fmt.Sprintf("CREATE USER %v", name)
-
-	//append login_name if not null
 	if login_name != "" {
 		statement += fmt.Sprintf(" LOGIN_NAME = '%s'", login_name)
+	}
+	if email != "" {
+		statement += fmt.Sprintf(" EMAIL = '%s'", email)
 	}
 
 	_, err := db.Exec(statement)
@@ -67,11 +80,59 @@ func resourceSnowflakeUserRead(d *schema.ResourceData, meta interface{}) error {
 		return err
 	}
 	d.Set("name", userInfo.name)
+	d.Set("login_name", userInfo.login_name)
+	d.Set("email", userInfo.email)
+
 	return nil
 }
 
 func resourceSnowflakeUserUpdate(d *schema.ResourceData, meta interface{}) error {
-	// Update comment
+	db := meta.(*sql.DB)
+	name := d.Id()
+	exists, err := sqlObjExists(db, "users", name, "account")
+
+	if err != nil {
+		return err
+	}
+	if exists == false {
+		return fmt.Errorf("User %s does not exist", d.Id())
+	}
+	// Rather than issue a single alter user statement for all possible
+	// changes issue an alter for each possible thing that has changed. Enable
+	// partial mode.
+	d.Partial(true)
+	if d.HasChange("name") {
+		// check that the rename target does not exist
+		exists, err := sqlObjExists(db, "users", d.Get("name").(string), "account")
+		if err != nil {
+			return err
+		}
+		if exists == true {
+			return fmt.Errorf("Cannot rename %v to %v, %v already exists", d.Id(), d.Get("name"), d.Get("name"))
+		}
+		statement := fmt.Sprintf("ALTER USER %v RENAME TO %v", d.Id(), d.Get("name"))
+		if _, err := db.Exec(statement); err != nil {
+			return err
+		}
+		d.SetPartial("name")
+		d.SetId(d.Get("name").(string))
+
+	}
+	if d.HasChange("email") {
+		statement := fmt.Sprintf("ALTER USER %v SET EMAIL = '%v'", d.Id(), d.Get("email"))
+		if _, err := db.Exec(statement); err != nil {
+			return err
+		}
+		d.SetPartial("email")
+	}
+	if d.HasChange("login_name") {
+		statement := fmt.Sprintf("ALTER USER %v SET LOGIN_NAME = '%v'", d.Id(), d.Get("login_name"))
+		if _, err := db.Exec(statement); err != nil {
+			return err
+		}
+		d.SetPartial("login_name")
+	}
+	d.Partial(false)
 	return nil
 }
 
